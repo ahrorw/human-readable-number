279bee1fced945000b3c32485d4c3b10f3f5da32
ahrorw <32160173+ahrorw@users.noreply.github.com>
2020-10-12 12:00:37 +0300

    feat: finish Human readable number Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+
 module.exports = function toReadable (number) {
-  
+  let array = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let arrayDec = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let strText = '';
+  let numStr = String(number);
+
+  if (number > 99) {
+    strText += array[Number(numStr.substr(-3,1))]  + ' hundred ';
+    numStr = numStr.substr(-2);
+  }
+
+  if (Number(numStr) > 19) {
+    strText += arrayDec[Number(numStr[0]) - 2] + ' ';
+    numStr = (numStr[1] === '0') ? '' : numStr[1];
+  } else if (numStr === '00') {
+    numStr = '';
+  }
+
+  strText += (numStr === '') ? '' : array[Number(numStr)];
+
+  return strText.trim();
 }
